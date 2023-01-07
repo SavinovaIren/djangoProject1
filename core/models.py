ef7d92bcf6447cded4042d2685c1c8016dc12cd6
SavinovaIren <irka887@gmail.com>
2023-01-07 17:02:47 +0300

    add Bot

--- core/models.py
+++ core/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import AbstractUser
-
+from django.db import models
 
 class User(AbstractUser):
+    birth_date = models.DateField(null=True)
+
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
