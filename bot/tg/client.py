e1a12fb95834ff1337b3f3509f13f0a21a6a0a0b
SavinovaIren <103447690+SavinovaIren@users.noreply.github.com>
2023-01-08 12:20:03 +0300

    add tests

--- bot/tg/client.py
+++ bot/tg/client.py
@@ -12,7 +12,7 @@ class TgClient:
 
     def get_updates(self, offset: int = 0, timeout: int = 60) -> GetUpdatesResponse:
         url = self.get_url("getUpdates")
-        response = requests.get(url, params={"offset": offset, "timeout": timeout})
+        response = requests.get(url, params={"offset": offset, "timeout": timeout, "allowed_updates": ["update_id", "message"]})
         return GetUpdatesResponse(**response.json())
 
     def send_message(self, chat_id: int, text: str) -> SendMessageResponse:
