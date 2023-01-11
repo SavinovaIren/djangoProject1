1808240849caf598c7e2c10ee1716187e7b439f3
SavinovaIren <103447690+SavinovaIren@users.noreply.github.com>
2023-01-12 02:24:41 +0300

    add Bot

--- bot/models.py
+++ bot/models.py
@@ -1,18 +1,30 @@
-from django.core.validators import MinLengthValidator
 from django.db import models
-from django.utils.crypto import get_random_string
 
 # Create your models here.
+from core.models import User
+
+
 class TgUser(models.Model):
-    tg_chat_id = models.BigIntegerField()
-    tg_user_id = models.BigIntegerField(unique=True)
-    tg_username = models.CharField(max_length=32, validators=[MinLengthValidator(5)])
-    user = models.ForeignKey('core.User', null=True, on_delete=models.CASCADE)
-    verification_code = models.CharField(max_length=10, unique=True)
-
-
-    def generate_verification_code(self) -> str:
-        code = get_random_string(10)
-        self.verification_code = code
-        self.save()
-        return code
+    chat_id = models.BigIntegerField(
+        verbose_name="Chat ID",
+        unique=True
+    )
+    username = models.CharField(
+        verbose_name="Username",
+        max_length=255,
+        null=True,
+        blank=True,
+        default=None
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+        default=None
+    )
+    verification_code = models.CharField(
+        max_length=32,
+        null=True,
+        blank=True,
+        default=None
+    )
--- bot/serializers.py
+++ bot/serializers.py
@@ -1,9 +1,24 @@
-from rest_framework import  serializers
+from typing import Any
+
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from bot.models import TgUser
 
 
 class TgUserSerializer(serializers.ModelSerializer):
+    verification_code = serializers.CharField(write_only=True)
+    tg_id = serializers.SlugField(source="chat_id", read_only=True)
+
     class Meta:
         model = TgUser
-        fields=('verification_code',)
+        fields = ("tg_id", "username", "verification_code", "user_id")
+        read_only_fields = ("tg_id", "username", "user_id")
+
+    def validate(self, attrs: dict[str, Any]):
+        validation_code = attrs.get("verification_code")
+        tg_user = TgUser.objects.filter(verification_code=validation_code).first()
+        if not tg_user:
+            raise ValidationError({"verification_code": "fields is incorrect"})
+        attrs["tg_user"] = tg_user
+        return attrs
